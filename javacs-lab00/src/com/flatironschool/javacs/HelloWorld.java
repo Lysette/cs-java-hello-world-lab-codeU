a48643a59435e24e814dcd72c4d6872f144e7082
Lysette <isjochan@gmail.com>
2016-03-27 22:55:56 -0400

    finished HelloWorld

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -3,9 +3,13 @@ package com.flatironschool.javacs;
 public class HelloWorld {
     public static Double getVersion() {
         // I won't work!
+		String version = System.getProperty("java.specification.version");
+		return Double.parseDouble(version);
     }
 
     public static void main(String[] args) {
-	    // you can test the output of getVersion() here
+	    double version = getVersion();
+		System.out.println(version);
+		// you can test the output of getVersion() here
     }
 }
